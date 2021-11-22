673177d54aa6ec90bce1e3b725b7d6385c80c74e
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-11-22 18:48:01 +0100

    ostatak

---
+++ vežbe1/primer1/entity.jl
@@ -0,0 +1,41 @@
+# primer 1
+
+mutable struct Entity
+    genes::Array{Int64, 1}
+    fitness::Int64
+end
+
+function generateEntity(genesLength)
+    return Entity(rand(0:1,genesLength),0)
+end
+
+function calculateFitness!(entity, fitValue)
+    entity.fitness = 0
+    for i in 1:length(entity.genes)
+        entity.fitness += entity.genes[i]
+    end
+    entity.fitness = abs(entity.fitness - fitValue)
+end
+
+function crossover!(entity1, entity2, crossoverPoint)
+    for i in 1:crossoverPoint
+        x = entity1.genes[i]
+        entity1.genes[i] = entity2.genes[i]
+        entity2.genes[i] = x
+    end
+end
+
+function mutation!(entity, mutationPercentage)
+    if rand(Float64) < mutationPercentage
+        mutationPoint = rand(1:length(entity.genes))
+        entity.genes[mutationPoint] = 1 - entity.genes[mutationPoint]
+		# invertovanje po matematički
+    end
+end
+
+function printEntity(entity)
+    for i in 1:length(entity.genes)
+        print(entity.genes[i])
+    end
+    print(" $(entity.fitness)\n")
+end
---
+++ vežbe1/primer1/geneticAlgorithm.jl
@@ -0,0 +1,31 @@
+# primer 1
+
+include("population.jl")
+
+function geneticAlgorithm!(data, fitValue, selectionSize, crossoverPoint, mutationPercentage, repeatSize) 
+    calculatePopulationFitness!(data, fitValue)
+    bestFit = data[1].fitness
+
+    repeatCount = 1
+    popGen = 0
+
+    while (data[1].fitness > 0) && (repeatCount < repeatSize)
+        data = selectPopulation(data, selectionSize)
+        data = crossoverPopulation(data, crossoverPoint)
+        mutatePopulation!(data, mutationPercentage)
+        calculatePopulationFitness!(data, fitValue)
+		
+		popGen += 1
+        print("Generacija $popGen, bestFit $bestFit, broj ponavljanja $repeatCount ")
+        
+		if bestFit == data[1].fitness
+            repeatCount += 1
+        else
+            bestFit = data[1].fitness
+            repeatCount = 1
+        end
+
+        
+    end
+    return popGen, repeatCount, population
+end
