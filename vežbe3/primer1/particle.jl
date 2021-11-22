4e60cacff314716e3052f6e61858e77b42e5e373
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-11-22 19:34:26 +0100

    čestica

--- vežbe3/primer1/particle.jl
+++ vežbe3/primer1/particle.jl
@@ -1,11 +1,11 @@
 # primer 1
 
 mutable struct Particle
-    values::Array{Int64, 1}											# pozicije do sad
-    currentValue::Int64												# trenutna pozicija (zbir dosadašnjih pozicija)
-    velocity::Array{Float64, 1}										# brzina
-    bestValues::Array{Float64, 1}									# najbolje pozicije čitavog rešenja
-    bestValue::Int64												# njena najbolja pozicija
+    values::Array{Int64, 1}					# pozicije do sad
+    currentValue::Int64						# trenutna pozicija (zbir dosadašnjih pozicija)
+    velocity::Array{Float64, 1}					# brzina
+    bestValues::Array{Float64, 1}				# najbolje pozicije čitavog rešenja
+    bestValue::Int64						# njena najbolja pozicija
 end
 
 function updateValue!(particle)
@@ -24,12 +24,12 @@ end
 function printParticle(particle)
     for i in 1:length(particle.values)
         if i < length(particle.values)
-            print(particle.values[i], " + ")						# plus između sabiraka
+            print(particle.values[i], " + ")			# plus između sabiraka
         else
-            print(particle.values[i], " = ")						# jednako pre rešenja
+            print(particle.values[i], " = ")			# jednako pre rešenja
         end
     end
-    println(particle.currentValue)									# rešenje (tj zbir)
+    println(particle.currentValue)				# rešenje (tj zbir)
 end
 
 # Vi = Cv*Vi + Cp*Rp*(Pi-Xi) + Cg*Rg*(g-Xi)
@@ -43,9 +43,9 @@ function updateVelocity!(particle, globalBestParticle, maxVelocity)
     for i in 1:length(particle.velocity)
         particle.velocity[i] = 1.0 * particle.velocity[i]  +  2.0 * rp * (particle.bestValues[i] - particle.values[i])  +  2.0 * rg * (globalBestParticle.values[i] - particle.values[i])
 
-        if particle.velocity[i] > maxVelocity						# ako je veća od najveće dozvoljene brzine, smanji na tu najveću dozvoljenu
+        if particle.velocity[i] > maxVelocity			# ako je veća od najveće dozvoljene brzine, smanji na tu najveću dozvoljenu
             particle.velocity[i] = maxVelocity
-        elseif particle.velocity[i] < -maxVelocity					# ako je manja od najmanje dozvoljene brzine, povećaj na tu najmanju dozvoljenu
+        elseif particle.velocity[i] < -maxVelocity		# ako je manja od najmanje dozvoljene brzine, povećaj na tu najmanju dozvoljenu
             particle.velocity[i] = -maxVelocity
         end
     end
@@ -53,11 +53,11 @@ end
 
 function updatePosition!(particle, targetValue)
     for i in 1:length(particle.values)
-        particle.values[i] += trunc(Int64, particle.velocity[i])	# pozicija = pozicija + brzina čestice (s=v*t, a ovde je t=1 => s=v)
+        particle.values[i] += trunc(Int64, particle.velocity[i])
     end
     updateValue!(particle)
 
-	# ako je razlika između trenutne vrednosti i ciljne vrednosti manja od razlike najbolje vrednosti i ciljne vrednosti, sačuvaj trenutnu vrednost kao najbolju vrednost do sad
+    # ako je razlika između trenutne vrednosti i ciljne vrednosti manja od razlike najbolje vrednosti i ciljne vrednosti, sačuvaj trenutnu vrednost kao najbolju vrednost do sad
     if abs(particle.currentValue - targetValue) < abs(particle.bestValue - targetValue)
         particle.bestValues = copy(particle.values)
         particle.bestValue = particle.currentValue
