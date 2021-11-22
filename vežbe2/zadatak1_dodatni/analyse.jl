41132604759ddf6497a16d67c792d255b360da1a
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-11-22 19:37:47 +0100

    analiza

--- vežbe2/zadatak1_dodatni/analyse.jl
+++ vežbe2/zadatak1_dodatni/analyse.jl
@@ -3,7 +3,7 @@ include("geneticAlgorithm.jl")
 function calculateAverage(data, elitePercentage, crossoverPoint, mutationPercentage, num)
     averageGen = 0.0
     averageFit = 0.0
-    for i in 1:num											# num -> broj iteracija
+    for i in 1:num								# num -> broj iteracija
         gen, pop = geneticAlgorithm!(data, elitePercentage, crossoverPoint, mutationPercentage)
         averageGen += gen
         averageFit += pop[1].fitness
@@ -23,7 +23,7 @@ function findBestElitePercentage(data, eliteRange, crossoverPoint, mutationPerce
         push!(resultsFit, averageFit)
         push!(resultsGen, averageGen)
     end
-    minIndex = argmin(resultsFit)							# traži indeks generacije sa najmanjim fitnessom
+    minIndex = argmin(resultsFit)						# traži indeks generacije sa najmanjim fitnessom
     optimalValue = resultsFit[minIndex]						# i ovako izvlačimo vrednog tog najmanjeg fitnessa
     return minIndex, optimalValue, resultsFit, resultsGen
 end
@@ -36,7 +36,7 @@ function findBestCrossoverAndMutation(data, elitePercentage, crossoverRange, mut
         crossoverPoint = crossoverRange[i]					# crossoverPoint je 2, 3, 4, 5
 
         for j in 1:length(mutationRange)					# 0, 0.01, ..., 0.3 -> koliko su učestale mutacije
-            mutationPercentage = mutationRange[j]			# change: mutationPoint -> mutationPercentage    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            mutationPercentage = mutationRange[j]				# change: mutationPoint -> mutationPercentage  !!!
             dataCopy = deepcopy(data)
             averageGen, averageFit = calculateAverage(dataCopy, elitePercentage, crossoverPoint, mutationPercentage, num)
             resultsFit[i, j] = averageFit
@@ -47,6 +47,6 @@ function findBestCrossoverAndMutation(data, elitePercentage, crossoverRange, mut
     minCrossoverIndex = coordinates[1]						# prva koordinata je tačka za crossover (for j in 1:length(mutationRange))
     minMutationIndex = coordinates[2]						# druga koordinata je tačka do koje geni jedinke mutiraju (for j in 1:length(mutationRange))
     optimalValue = resultsFit[minCrossoverIndex, minMutationIndex]
-															# i samo kopiraš vrednost fitnessa koji se nalazi na tim koordinatama
+										# i samo kopiraš vrednost fitnessa koji se nalazi na tim koordinatama
     return minCrossoverIndex, minMutationIndex, optimalValue, resultsFit, resultsGen
 end
