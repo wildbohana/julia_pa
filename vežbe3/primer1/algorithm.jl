89da68ff33f750696e47b28fa25ee555139357e7
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-11-22 19:32:40 +0100

    pso

--- vežbe3/primer1/algorithm.jl
+++ vežbe3/primer1/algorithm.jl
@@ -15,19 +15,19 @@ function PsoAlgorithm!(swarm, targetValue, maxIteration, maxVelocity)
 
     for i in 1:maxIteration
         if globalBestParticle == targetValue
-			# kad nađe traženu vrednost, odštampa je i odmah izlazi iz cele funkcije
+	    # kad nađe traženu vrednost, odštampa je i odmah izlazi iz cele funkcije
             printSwarm(swarm)
-        	println("-- RESENJE --")
+            println("-- RESENJE --")
             printParticle(globalBestParticle)
             println("RESENJE PRONADJENO POSLE $i ITERACIJA.")
             return
         end	
-		# ako u ovoj iteraciji nije našao traženo rešenje, onda računamo novo globalno najbolje
+	# ako u ovoj iteraciji nije našao traženo rešenje, onda računamo novo globalno najbolje
         updateSwarmVelocity!(swarm, globalBestParticle, maxVelocity)
         updateSwarmPosition!(swarm, targetValue)    
         globalBestParticle = calculateGlobalBestParticle(swarm, targetValue, globalBestParticle)
     end
-	# ako ni jedna iteracija nije pronašla traženo rešenje, onda štampaj globalno najbolje
+    # ako ni jedna iteracija nije pronašla traženo rešenje, onda štampaj globalno najbolje
     printSwarm(swarm)
     println("\nGLOBALNO RESENJE")
     printParticle(globalBestParticle)
