fa00196efba64fd7e9e49d766e3dd01a68a5f3fe
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-11-22 19:41:06 +0100

    genetski

--- vežbe2/zadatak2/geneticAlgorithm.jl
+++ vežbe2/zadatak2/geneticAlgorithm.jl
@@ -3,12 +3,12 @@
 include("population.jl")
 
 function converge(bestFits)
-    len = length(bestFits)										# linija 121, bestFits jeste fitness najboljeg
+    len = length(bestFits)
     if bestFits[len] < 0.01
         return true
     elseif len < 3
         return false
-    elseif bestFits[len - 2] == bestFits[len]					# tri uzastopna ponavljanja istog rešenja, uslov zasićenja
+    elseif bestFits[len - 2] == bestFits[len]				# tri uzastopna ponavljanja istog rešenja, uslov zasićenja
         return true
     else
         return false
@@ -22,7 +22,7 @@ function geneticAlgorithm!(data, elitePercentage, crossoverPoint1, crossoverPoin
 
     # Ako je broj jedinki elite neparan, dodaj jos jednu jedinku do parnog broja
     eliteSize = eliteSize + (populationSize - eliteSize) % 2
-	# JEBO MAMU SVOJU ZAŠTO NIKO NIJE REKAO DA POPULATIONSIZE MORA BITI PARAN BROJ DA BI POLA KODA KURCU VALJALO?
+   # JEBO MAMU SVOJU ZAŠTO NIKO NIJE REKAO DA POPULATIONSIZE MORA BITI PARAN BROJ DA BI POLA KODA KURCU VALJALO?
 
     bestFits = [data[1].fitness]
 
