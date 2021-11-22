d74040b08583229ef2eaab4013a0af72e4fff47c
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-11-22 18:54:26 +0100

    main

---
+++ vežbe3/primer1/primer1.jl
@@ -0,0 +1,14 @@
+# primer 1
+
+include("algorithm.jl")
+
+numberOfParticles = 10
+numberOfOperands = 5		# n
+minRang = 100
+maxRang = 200
+targetValue = 200			# s
+maxIteration = 2000
+maxVelocity = 10
+
+swarm = generateSwarm(numberOfParticles, numberOfOperands, minRang, maxRang)
+PsoAlgorithm!(swarm, targetValue, maxIteration, maxVelocity)
